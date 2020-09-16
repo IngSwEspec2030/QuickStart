c480a211f41300d2b5f17078e0a3f4cc4b38e24a
lore <chcindyl3@gmail.com>
2020-09-15 22:03:46 -0500

    Cargue de imagenes a AWS 15/09/2020

--- QuickTaskApp/Views/TaskTakePage.xaml.cs
+++ QuickTaskApp/Views/TaskTakePage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using Plugin.Media;
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using QuickTaskApp.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +21,9 @@ namespace QuickTaskApp.Views
     public partial class TaskTakePage : ContentPage
     {
         private MediaFile _mediaFile;
+        private static string PutAWSAccessKey = "AKIAIGDCUCVJ6JHKEYXA";
+        private static string PutAWSSecretKey= "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";
+        private Stream stream;
 
         public TaskTakePage(Item item)
         {
@@ -46,9 +54,10 @@ namespace QuickTaskApp.Views
 
             DisplayAlert("File Location", _mediaFile.Path, "OK");
 
+            await AWSUploadPic(_mediaFile.Path, System.IO.Path.GetFileName(_mediaFile.Path));
             FileImage.Source = ImageSource.FromStream(() =>
             {
-                var stream = _mediaFile.GetStream();
+                stream = _mediaFile.GetStream();
                 _mediaFile.Dispose();
                 return stream;
             });
@@ -67,11 +76,51 @@ namespace QuickTaskApp.Views
 
             if (_mediaFile == null)
                 return;
-            LocalPath.Text = _mediaFile.Path;
+            //LocalPath.Text = _mediaFile.Path;
+
+            await AWSUploadPic(_mediaFile.Path, System.IO.Path.GetFileName(_mediaFile.Path));
             FileImage.Source = ImageSource.FromStream(() =>
             {
                 return _mediaFile.GetStream();
             });
+
+        }
+
+        public static async Task AWSUploadPic(string filePath, string requiredFileName)
+        {
+            try
+            {
+
+                AWSCredentials creds = new BasicAWSCredentials(PutAWSAccessKey,
+                                                                   PutAWSSecretKey);
+
+                var client = new AmazonS3Client(creds, RegionEndpoint.USEast2);    // "USWest2" replace your code(Amazon)
+
+                var po = new PutObjectRequest();
+                po.CannedACL = S3CannedACL.PublicReadWrite;
+                po.FilePath = filePath;
+                po.BucketName = "quicktask";
+                po.Key = requiredFileName;
+
+                var p = await client.PutObjectAsync(po);
+
+
+                Console.WriteLine("Upload completed");
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    throw new Exception("Check the provided AWS Credentials.");
+                }
+                else
+                {
+                    throw new Exception("Error occurred: " + amazonS3Exception.Message);
+                }
+            }
         }
     }
 }
